cc6de691686712c1e3e01cdd3fdfaf9278d2e816
codyalvarez <jedi.alvarez805@gmail.com>
2020-11-09 12:22:28 -0500

    session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
-    enable :sesssions
+    enable :sessions
     set :session_secret, "the_collective_secret"
   end
 
