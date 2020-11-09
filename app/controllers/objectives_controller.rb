dafd7760c128a3ab4128133ce771c7709723ed90
codyalvarez <jedi.alvarez805@gmail.com>
2020-11-09 14:11:29 -0500

    objectives

--- app/controllers/objectives_controller.rb
+++ app/controllers/objectives_controller.rb
@@ -17,5 +17,78 @@ class ObjectivesController < ApplicationController
     end
   end
 
+  post '/objectives' do
+    if logged_in?
+      if params[:content] == ""
+        redirect to "/objectives/new"
+      else
+        @objective = current_user.objectives.build(content: params[:content])
+        if @objective.save
+          redirect to "/objectives/#{@objective.id}"
+        else
+          redirect to "/objectives/new"
+        end
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/objectives/:id' do
+    if logged_in?
+      @objective = Objective.find_by_id(params[:id])
+      erb :'objectives/show_objective'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/objectives/:id/edit' do
+    if logged_in?
+      @objective = Objective.find_by_id(params[:id])
+      if @objective && @objective.user == current_user
+        erb :'objectives/edit_objective'
+      else
+        redirect to '/objectives'
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
+  patch '/objectives/:id' do
+    if logged_in?
+      if params[:content] == ""
+        redirect to "/objectives/#{params[:id]}/edit"
+      else
+        @objective = Objective.find_by_id(params[:id])
+        if @objective && @objective.user == current_user
+          if @objective.update(content: params[:content])
+            redirect to "/objectives/#{@objective.id}"
+          else
+            redirect to "/objectives/#{@object.id}/edit"
+          end
+        else
+          redirect to '/onjectives'
+        end
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
+  delete '/objectives/:id/delete' do
+    if logged_in?
+      @objective = Objective.find_by_id(params[:id])
+      if @objective && @objective.user == current_user
+        @objective.delete
+      end
+      redirect to '/objectives'
+    else
+      redirect to '/login'
+    end
+  end
+    
+
 end
 
